12403f7a5c893585e8fd485a0f19dfff6253cad2
Rossie <rossie.g@gmail.com>
2018-01-24 00:55:13 +0100

    Added comments js

---
+++ public/js/ip-comments.js
@@ -0,0 +1,12 @@
+$(function(){
+    // https://stackoverflow.com/questions/399867/custom-events-in-jquery     
+
+    ///////////////////////////////////////////////////
+    // Comments for the IP address
+    ///////////////////////////////////////////////////
+    $(document).on('iplookup.ipfetched', function(evt, ip, result){
+        //TODO: fetch comments from server (ajax)
+        //TODO: show comments sections
+        //TODO: saving and display new comment (ajax)
+    });
+});
