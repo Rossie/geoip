a0d71cabc6182e79e414cc37161d0cc52dc23e79
Rossie <rossie.g@gmail.com>
2018-01-24 09:38:59 +0100

    Added comment section on page

--- public/js/ip-comments.js
+++ public/js/ip-comments.js
@@ -5,8 +5,20 @@ $(function () {
     // Comments for the IP address
     ///////////////////////////////////////////////////
     $(document).on('iplookup.ipfetched', function (evt, ip, result) {
+        $('#comments_title_ip').html(ip);
         //TODO: fetch comments from server (ajax)
         //TODO: show comments sections
         //TODO: saving and display new comment (ajax)
+
+        $('#comment_panel').delay(0).slideDown();
     });
-});
+
+    $('#btnCommentPost').click(function(evt){
+        var comment = $('#txtComment').val().trim();
+        if (!comment) return;
+
+        $('#commentForm textarea, #commentForm button').prop('disabled', true);
+        console.log('comment', comment);
+    });
+
+}); 
