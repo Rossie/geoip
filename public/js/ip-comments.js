41bb7abd7a39912a1e7664bd3e6afe2dadf89a6b
Rossie <rossie.g@gmail.com>
2018-01-30 17:54:44 +0100

    Fixed comment date on post

--- public/js/ip-comments.js
+++ public/js/ip-comments.js
@@ -1,35 +1,11 @@
 $(function () {
     // https://stackoverflow.com/questions/399867/custom-events-in-jquery     
-
+    
     var _ip = window.viewIp;
     ///////////////////////////////////////////////////
     // Comments for the IP address
     ///////////////////////////////////////////////////
-    $(document).on('iplookup.ipfetched', function (evt, ip, result) {
-        $('#commentsTitleIp').html(ip);
-        _ip = ip;
-        fetchComments();
-    });
-
-    function fetchComments() {
-        $('.subscriber.comment-invoked').trigger('comment.invoked');
-        $.ajax({
-            dataType: "json",
-            url: '/comment-api/' + _ip,
-            success: function (result) {
-                if (result.result == "ok") {
-                    $('.subscriber.comment-data').trigger('comment.data', [result.comments]);
-                }
-                else {
-                    $('.subscriber.comment-error').trigger('comment.error', [result.message]);
-                }
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.error(errorThrown);
-            }
-        });
-    }
-
+    
     $('#btnCommentPost').click(function (evt) {
         var comment = $('#txtComment').val().trim();
         if (!comment) return;
@@ -45,7 +21,7 @@ $(function () {
             url: '/comment-api/' + _ip,
             success: function (result) {
                 if (result.result === "ok") {
-                    $('.subscriber.comment-post').trigger('comment.post', [result.comment]);
+                    $('.subscriber.comment-post').trigger('comment.post-ok', [result.comment]);
                 }
                 else {
                     $('.subscriber.comment-error').trigger('comment.error', [result.message]);
@@ -60,42 +36,27 @@ $(function () {
     //////////////////////////////
     // Comment list
     //////////////////////////////
-    $('#commentList').on('comment.invoked', function (evt) {
-        $('#commentPanel').fadeOut('fast');
-    })
-        .on('comment.data', function (evt, result) {
-            var qCommentList = $(this);
-            qCommentList.empty(); // remove all contents
+    $('#commentList')
+        .on('comment.post-ok', function (evt, comment) {
+            // convert date
+            comment.post_date = (new Date(comment.post_date)).toString();
 
-            // COMMENT please use templating (e.g. mustache.js), don't put HTML inside JS code
-            result.forEach(function (comment) {
-                qCommentList.append(
-                    '<li class="list-group-item">' +
-                    '<h6 class="text-muted">' + comment.post_date + '</h6>' +
-                    '<p>' + comment.comment + '</p>' +
-                    '</li>'
-                );
-            });
+            // set up Mustache.js on templates - https://github.com/janl/mustache.js
+            var commentTemplate = $('#commentTemplate').html();
+            Mustache.parse(commentTemplate); 
 
-            $('#commentPanel').delay(0).slideDown();
-        })
-        .on('comment.post', function (evt, comment) {
-            // console.log('comment.post', result);
-            $(this).append(
-                '<li class="list-group-item">' +
-                '<h6 class="text-muted">' + comment.post_date + '</h6>' +
-                '<p>' + comment.comment + '</p>' +
-                '</li>'
-            );
+            var commentHtml = Mustache.render(commentTemplate, comment);
+            $(this).append(commentHtml);
         });
 
     //////////////////////////////
     // Comment POST form
     //////////////////////////////
-    $('#commentForm').on('comment.post-invoked', function (evt) {
-        $('#commentForm textarea, #commentForm button').prop('disabled', true);
-    })
-        .on('comment.post', function (evt, comment) {
+    $('#commentForm')
+        .on('comment.post-invoked', function (evt) {
+            $('#commentForm textarea, #commentForm button').prop('disabled', true);
+        })
+        .on('comment.post-ok', function (evt, comment) {
             $('#commentForm textarea, #commentForm button').prop('disabled', false);
             if (comment.id) {
                 $('#txtComment').val(''); // empty textarea
